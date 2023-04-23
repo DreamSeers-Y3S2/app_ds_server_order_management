5671bd5df5e2709e74be7968066917c3c7143bc9
ChathurikaThathsarani <it20603168@my.sliit.lk>
2023-04-23 20:44:59 +0530

    Implement backend for order management

---
+++ controllers/orderController.js
@@ -0,0 +1,73 @@
+const Order = require("../models/orderModel");
+const Cart = require("../models/cartModel");
+const asyncHandler = require("express-async-handler");
+
+const getCustomerOrders = asyncHandler(async (req, res) => {
+  const items = await Order.find({ customer: req.params.id });
+  res.json(items);
+});
+
+const getAdminOrders = asyncHandler(async (req, res) => {
+  const items = await Order.find();
+  res.json(items);
+});
+
+const createOrder = asyncHandler(async (req, res) => {
+  var status = "pending";
+  var products = "";
+  var i = 0;
+  const { customer, total } = req.body;
+  var orderID = Math.floor(Math.random() * 100000);
+
+  if (!customer || !total) {
+    res.status(400);
+    throw new Error("Failed creating order");
+  } else {
+    const items = await Cart.find({
+      customer: customer,
+    });
+
+    while (i < items.length) {
+      products = products + items[i].productName;
+      if (i != items.length - 1) {
+        products = products + " , ";
+      }
+      i++;
+    }
+
+    const order = new Order({
+      customer,
+      products,
+      orderID,
+      total,
+      status,
+    });
+
+    const createdOrder = await order.save();
+
+    res.status(201).json(createdOrder);
+  }
+});
+
+const updateOrderStatus = asyncHandler(async (req, res) => {
+  const { status } = req.body;
+
+  const order = await Order.findById(req.params.id);
+
+  if (order) {
+    order.status = status;
+
+    const updatedOrder = await order.save();
+    res.json(updatedOrder);
+  } else {
+    res.status(404);
+    throw new Error("Order not found");
+  }
+});
+
+module.exports = {
+  createOrder,
+  getCustomerOrders,
+  getAdminOrders,
+  updateOrderStatus,
+};
---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const app = express();
+const orderRoutes = require("./routes/orderRoutes");
+const connectDB = require("./config/db");
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
+
+dotenv.config();
+connectDB();
+app.use(express.json());
+app.use("*", cors());
+
+app.get("/", (req, res) => {
+  res.send("Order API is Running");
+});
+
+app.use("/order", orderRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5008;
+app.listen(PORT, console.log(`Order Server Started on port ${PORT}..`));
