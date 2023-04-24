22e2cd1e5647f724e930293a74f1d3547f6e9477
Thamindu Chankana Gamage <73847570+ThaminduChankana@users.noreply.github.com>
2023-04-25 02:29:00 +0530

    added docker file

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,19 +1,12 @@
 const express = require("express");
 
-const {
-  createOrder,
-  getCustomerOrders,
-  getAdminOrders,
-  updateOrderStatus,
-} = require("../controllers/orderController");
+const { createOrder, getCustomerOrders, getAdminOrders, updateOrderStatus } = require("../controllers/orderController");
 const { protectCustomer } = require("../middleware/authCustomerMiddleware");
 const { protect } = require("../middleware/authAdminMiddleware");
 const router = express.Router();
 
 router.route("/create").post(protectCustomer, createOrder);
-router
-  .route("/get-customer-orders/:id")
-  .get(protectCustomer, getCustomerOrders);
+router.route("/get-customer-orders/:id").get(protectCustomer, getCustomerOrders);
 router.route("/get-admin-orders").get(protect, getAdminOrders);
 
 router.route("/order-admin/:id").put(protect, updateOrderStatus);
